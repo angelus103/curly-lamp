ca03bd7128b4d5246779771cebd29ff5b5489f74
B14-AINF <B14@academico.unab.ad>
2022-10-19 11:55:05 -0500

    first commit

---
+++ trabajo.py
@@ -0,0 +1,16 @@
+class ciclista:
+    def __init__(self, nombre, edad, colorbicicleta, nubes, cascocolor):
+        self.nombre = nombre
+        self.edad = edad
+        self.colorbicicleta = colorbicicleta
+        self.nubes = nubes
+        self.cascocolor = cascocolor
+pl = ciclista("juan", 10, "verde", "dos", "verde")
+print(pl.nombre)
+print(pl.edad)
+print(pl.colorbicicleta)
+print(pl.nubes)
+print(pl.cascocolor)
+
+        
+        
